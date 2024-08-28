4431b04ab01ff906a2bae1f3e1d14e26b348cbc0
Syeda Maryam Ahmed <smaryamahmed2006@gmail.com>
2024-08-28 16:03:16 +0500

    commiting

--- router/Doner-router.js
+++ router/Doner-router.js
@@ -1,29 +1,21 @@
 import express from 'express';
 import multer from 'multer';
-import multerS3 from 'multer-s3';
-import AWS from 'aws-sdk';
+import path from 'path';
 import { addDonor, getAllDonors } from '../controllers/DonerControllers.js';
 
 const DonerRouter = express.Router();
 
-// AWS S3 Configuration
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+// Multer setup
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
 });
 
-// Multer S3 setup
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET_NAME, // Ensure you set this environment variable
-        acl: 'public-read', // Allows the file to be publicly accessible
-        key: function (req, file, cb) {
-            cb(null, `donors/${Date.now()}_${file.originalname}`);
-        }
-    })
-});
+const upload = multer({ storage: storage });
 
 DonerRouter.get('/doner', getAllDonors);
 DonerRouter.post('/doner', upload.single('image'), addDonor);
